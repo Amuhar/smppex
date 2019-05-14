44d9666f84cf7a9c63d67086f0d7c9b4ac21f2fa
a.mukharram <a.mukharram@fun-box.ru>
2019-05-14 21:04:27 +0300

    разработка.

--- lib/smppex/esme.ex
+++ lib/smppex/esme.ex
@@ -105,30 +105,6 @@ defmodule SMPPEX.ESME do
     end
   end
 
-  def start_link(host, port, {_module, _args} = mod_with_args, opts \\ []) do
-    transport = Keyword.get(opts, :transport, @default_transport)
-    timeout = Keyword.get(opts, :timeout, @default_timeout)
-    sock_opts = [:binary, {:packet, 0}, {:active, :once}]
-    esme_opts = Keyword.get(opts, :esme_opts, [])
-
-    case transport.connect(convert_host(host), port, sock_opts, timeout) do
-      {:ok, socket} ->
-        session_opts = {Session, [mod_with_args, esme_opts], :esme}
-
-        case TransportSession.start_link(socket, transport, session_opts) do
-          {:ok, pid} ->
-            {:ok, pid}
-
-          {:error, _} = error ->
-            transport.close(socket)
-            error
-        end
-
-      {:error, _} = error ->
-        error
-    end
-  end
-
   defp convert_host(host) when is_binary(host), do: Compat.to_charlist(host)
   defp convert_host(host), do: host
 end
